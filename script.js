35f3c2ee31150069bf842972ca700d4116eeb07b
shoyaa0815 <shoyaa0815@gmail.com>
2025-06-15 19:03:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,12 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
+
+  const toggleBtn = document.getElementById("menu-toggle");
+  const navLinks = document.getElementById("nav-links");
+
+  toggleBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+  });
+
+
