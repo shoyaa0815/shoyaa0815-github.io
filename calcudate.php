cdbdda056721118ab7ad7c73562a59d0654d83da
shoyaa0815 <shoyaa0815@gmail.com>
2025-08-04 10:55:03 +0700

    Add files via upload

---
+++ calcudate.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<form>in</form>
+<?php
+    echo date("Y")+543 . "<br>";
+    echo "Today is" . date("Y.M.D") . "<br>";
+    echo "Today is" . date("Y-M-D") . "<br>";
+    echo "Today is" . date(" l")
+?>
+
+
+
+
+</body>
+</html>
