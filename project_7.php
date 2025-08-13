1006b785f253c824e532a52b44e5bce3159cad46
shoyaa0815 <shoyaa0815@gmail.com>
2025-08-13 14:29:01 +0700

    Update project_7.php

--- project_7.php
+++ project_7.php
@@ -1,1 +1,17 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php
+    $a = 10;
+    $b = 10;
+    $c = $a + $b;
+     ?>
+</head>
+<body>
+    
+    <p>sum is<?php echo " $a + $b = $c" ?></p>
+</body>
+</html>
